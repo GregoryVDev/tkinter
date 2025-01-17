567a508298cea123ad3ec66087fffcae6acd3696
GregoryVDev <arexo@hotmail.fr>
2025-01-17 21:28:39 +0100

    Created submit, reset and quit buttons to be displayed below the inputs with the grid

--- grid.py
+++ grid.py
@@ -5,6 +5,14 @@ window = Tk()
 window.title("Exploration des grilles en Tkinter")
 # Mettre des dimensions pour la fenêtre
 window.geometry("600x400")
+
+def validate():
+    pass
+
+def reset():
+    pass
+
+
 # Ecrire un text dans la fenêtre
 label_firstname = Label(window, text="Votre prénom")
 # Afficher dans un grid (1 ligne, 1 colonne, en position sticky vers l'est avec un padding extérieur de 20px
@@ -20,5 +28,17 @@ text_firstname.grid(row=1, column=2)
 
 text_name = Entry(window)
 text_name.grid(row=2, column=2)
+
+# Créer le bouton "valider"
+submit = Button(window, text="Valider", command=validate)
+submit.grid(row=3, column=1)
+
+# Créer le bouton "réinisialiser"
+reset = Button(window, text="Réinisialiser", command=reset)
+reset.grid(row=3, column=2)
+
+# Créer le bouton "quitter"
+leave = Button(window, text="Quitter", command=window.quit)
+leave.grid(row=3, column=3)
 
 window.mainloop()
