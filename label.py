7fba499d3c421e479e5fd16a1fa29743fa9ddbaf
GregoryVDev <arexo@hotmail.fr>
2025-01-17 14:21:21 +0100

    Displayed the text in the window

--- label.py
+++ label.py
@@ -1,6 +1,10 @@
 # Création des fenêtres et des labels
 from tkinter import *
 window = Tk()
-window.geometry("800x500")
+window.geometry("1200x800")
+# Ecrire dans la fenêtre "Bonjour" sans que ça s'affiche
+label1 = Label(window, text="Bonjour")
+# Afficher l'écriture dans la fenêtre
+label1.pack()
 
 window.mainloop()
