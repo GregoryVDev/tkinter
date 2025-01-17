8cc1aefaa5795a1e3ead6d74b7735ef05090bdaa
GregoryVDev <arexo@hotmail.fr>
2025-01-17 14:41:17 +0100

    Created a quit button to exit the open window

--- label.py
+++ label.py
@@ -1,10 +1,15 @@
-# Création des fenêtres et des labels
+# Création des boutons d'action
 from tkinter import *
 window = Tk()
 window.geometry("1200x800")
 # Ecrire dans la fenêtre "Bonjour" sans que ça s'affiche
 label1 = Label(window, text="Bonjour")
-# Afficher l'écriture dans la fenêtre
+# Afficher le text dans la fenêtre
 label1.pack()
+
+# Créer un bouton afin de pouvoir quitter la fenêtre
+button_leave = Button(window, text="Quitter", command=window.quit)
+# Afficher le bouton quitter dans la fenêtre
+button_leave.pack()
 
 window.mainloop()
