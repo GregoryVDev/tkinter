59227ea3e2a7b8c03aaad07066f46339a21477e8
GregoryVDev <arexo@hotmail.fr>
2025-01-17 15:09:42 +0100

    Displayed an indoor space with ipadx at the button and edited the comment

--- label.py
+++ label.py
@@ -9,7 +9,7 @@ label1.pack()
 
 # Créer un bouton afin de pouvoir quitter la fenêtre
 button_leave = Button(window, text="Quitter", command=window.quit, width = 30, height=10)
-# Afficher le bouton quitter dans la fenêtre (mettre un slide pour mettre le bouton à gauche de la fenêtre)
-button_leave.pack(side=LEFT, padx = 100)
+# Afficher le bouton quitter dans la fenêtre (mettre un slide pour mettre le bouton à gauche de la fenêtre), un padding extérieur (padx) et un padding intérieur (ipadx)
+button_leave.pack(side=LEFT, padx = 300, ipadx = 400)
 
 window.mainloop()
