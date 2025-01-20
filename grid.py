52dbd21e3d8a2d23b7a83b07a56e22db68ed0ea4
GregoryVDev <arexo@hotmail.fr>
2025-01-20 17:25:50 +0100

    Added a border, a font, font size and a colour for the label name

--- grid.py
+++ grid.py
@@ -19,13 +19,13 @@ def reset():
     text_name.delete(0,END)
     text_firstname.delete(0,END)
 
-# Ecrire un text dans la fenêtre (bd = bordure).
+# Ecrire un text dans la fenêtre (bd = bordure)
 label_firstname = Label(window, text="Votre prénom", bd=2, font=("Arial", 15, "bold"), fg= "red")
 # Afficher dans un grid (1 ligne, 1 colonne, en position sticky vers l'est avec un padding extérieur de 20px
 label_firstname.grid(row=1, column=1, sticky = "E", padx = 20)
 
 
-label_name = Label(window, text="Votre nom")
+label_name = Label(window, text="Votre nom", bd=2, font=("Arial", 15, "bold"), fg= "red")
 label_name.grid(row=2,column=1, sticky = "E", padx = 20)
 
 # Ajoute un input à côté du text "Votre prénom"
