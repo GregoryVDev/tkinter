c8dc89b42b8fc06d242afcac978620055fd1a2a0
GregoryVDev <arexo@hotmail.fr>
2025-01-20 17:49:23 +0100

    Added a padding y for the label name and firstname

--- grid.py
+++ grid.py
@@ -22,11 +22,11 @@ def reset():
 # Ecrire un text dans la fenêtre (bd = bordure)
 label_firstname = Label(window, text="Votre prénom", bd=2, font=("Arial", 15, "bold"), fg= "red")
 # Afficher dans un grid (1 ligne, 1 colonne, en position sticky vers l'est avec un padding extérieur de 20px
-label_firstname.grid(row=1, column=1, sticky = "E", padx = 20)
+label_firstname.grid(row=1, column=1, sticky = "E", padx = 20, pady=15)
 
 
 label_name = Label(window, text="Votre nom", bd=2, font=("Arial", 15, "bold"), fg= "red")
-label_name.grid(row=2,column=1, sticky = "E", padx = 20)
+label_name.grid(row=2,column=1, sticky = "E", padx = 20, pady=15)
 
 # Ajoute un input à côté du text "Votre prénom"
 text_firstname = Entry(window)
