8fb23c1750a5b53b27a45f3c7eefa536b1803791
GregoryVDev <arexo@hotmail.fr>
2025-01-20 19:09:39 +0100

    Added background by modifying the position for frame right

--- frame.py
+++ frame.py
@@ -10,10 +10,17 @@ frame_top = Frame(master, bg= "#1a8cff")
 # Permet de positionner le frame
 frame_top.place(x=0, y=0, width=1300, height=200)
 
+
 # Permet d'ajouter un background
 frame_left = Frame(master, bg="#ff8000")
 # Permet de positionner le frame
 frame_left.place(x=0, y=200, width=200, height=500)
+
+
+# Permet d'ajouter un background
+frame_right = Frame(master, bg="#9dc8e3")
+# Permet de positionner le frame
+frame_right.place(x=200, y=200, width=1100, height=500)
 
 # Permet d'ouvrir la page
 master.mainloop()
